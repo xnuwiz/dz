67d7b2e4e6397e95f89a5c876ba44802f61b7c46
STUDENT24 <student24@Nur-Ordas-iMac-4.local>
2019-12-04 00:40:40 -0800

    vtoroi kommit

--- src/index.js
+++ src/index.js
@@ -6,34 +6,43 @@ import "mdbreact/dist/css/mdb.css";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import "./index.css";
 class App extends Component {
-    state = {};
-    render() {
-    return (
-        <React.Fragment>
-        <MDBContainer>
-            <MDBRow>
-            <MDBCol md="9"><Event  title="Meeting with John" time="10:00" /></MDBCol>
-            <MDBCol md="3"></MDBCol>
-            <MDBCol md="9"><Event  title="Call with Ana" time="11:00" /></MDBCol>
-            <MDBCol md="3"></MDBCol>
-            </MDBRow>
-        </MDBContainer>
-        </React.Fragment>
-    );
+    constructor(props) {
+      super(props);
+      this.state = {
+        events: [
+          { time: "10:00", title: "Breakfast with Simon" },
+          { time: "10:30", title: "Daily Standup Meeting (recurring)" },
+          { time: "11:00", title: "Call with HRs" }
+        ]
+      };
     }
-}
-
-class Event extends Component {
-    state = {};
-    render() {
+  render() {
     return (
       <React.Fragment>
-          <h3>{this.props.time} - {this.props.title}</h3>
-          <button>Change my title</button>
-          <button onClick={(this.props.title = "New Title")}>Change my title</button>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="9"> {this.state.events.map(event => (
+              <Event time={event.time} title={event.title} />
+            ))}
+            </MDBCol>
+            <MDBCol md="3" />
+          </MDBRow>
+        </MDBContainer>
       </React.Fragment>
     );
   }
 }
+
+class Event extends Component {
+    render() {
+      return (
+        <React.Fragment>
+          <h3>
+            {this.props.time} - {this.props.title}
+          </h3>
+        </React.Fragment>
+      );
+    }
+  }
 
 ReactDOM.render(<App />, document.getElementById("root"));
